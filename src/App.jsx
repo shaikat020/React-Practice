15bd88bb1d9c61b78b347895a5dd5ccb7f022960
shaikat020 <tahsin.azad.skt@gmail.com>
2025-10-02 21:09:47 +0600

    Feat: Using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
+// With useState:
+// It adds a state variable to a component.
+// State is data that can change over time (like input values, counters, toggles, fetched data).
+// When state changes, React re-renders the component with the new data
 
+import { useState } from "react";
 
 function App() {
-  let count= 0;
+  // const hook = useState(0);
+  //using destructor
+  let [ count, setCount]= useState(0);
+  // console.log(a, b);
+  // let count= 0;
   
 
   return (
     <>
       <h1>Count is {count}</h1>
-      <button onClick={()=> {count++;
-      console.log(count);
+      
+      <button onClick={()=> {
+        count++;
+        setCount(count);
+      
     }
       }>Inc</button>
-      <button onClick={()=> {count--;
-    console.log(count);  
-    }
+      <button onClick={()=> {
+        count--;
+        setCount(count);  
+      }
       }>Dec</button>
       
     </>
