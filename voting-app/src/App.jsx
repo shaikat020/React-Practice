6984f8fa8891a4bea2b4aeec6e82b97609a55ff4
shaikat020 <tahsin.azad.skt@gmail.com>
2025-10-04 18:34:36 +0600

    Feat: Put voting method in voting app

--- voting-app/src/App.jsx
+++ voting-app/src/App.jsx
@@ -11,10 +11,26 @@ function App() {
   let [ age, setAge]= useState(15);
   // console.log(a, b);
   // let count= 0;
+  let [votesA, setVotesA] = useState(0);
+  let [votesB, setVotesB] = useState(0);
+
+  const iseligible = age>=18;
+  let message = "";
+
+  if (votesA > votesB){
+    message = "Candidate A is leading"
+  }
+  else if (votesA < votesB){
+    message = "Candidate B is leading"
+  }
+  else if (votesA == votesB){
+    message = "It is a tie"
+  }
   
 
   return (
     <>
+    <h1><b>Welcome to Voting App</b></h1>
       <h1>Age is {age}</h1>
       
       <button onClick={()=> {
@@ -30,7 +46,25 @@ function App() {
       }>Dec</button>
 
       <p>{age < 18 ? 'Sorry, you are not eligible to vote.': 'Welcome! You are eligible to vote.'}</p>
+
+      {iseligible && (<>
+        <button onClick={()=>{
+          votesA++;
+          setVotesA(votesA);
+        }}>Vote for candidate A</button>
+        <button onClick={()=>{
+          votesB++;
+          setVotesB(votesB);
+        }}>Vote for candidate B</button>
+
+        <h2>Results:        </h2>
+        <p>Votes A : {votesA}</p>
+        <p>Votes B : {votesB}</p>
+        </>
+      )}
       
+      <h2>Remarks:</h2>
+      <p>{message}</p>
     </>
   )
 }
