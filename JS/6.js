c8e43529f3e70d4e680ad3a9cde1b4a49598c8e3
shaikat020 <tahsin.azad.skt@gmail.com>
2025-10-04 16:37:07 +0600

    Feat: Executing normal function and arrow function using IIFe

---
+++ JS/6.js
@@ -0,0 +1,10 @@
+//6. IIFE (Immediately Invoked Function Expressions)
+//a) Write a normal function IIFE.
+(function(){
+    console.log("This normal function is executed by IIFE")
+})();
+
+//b) Write an arrow function IIFE.
+(()=>{
+    console.log("This arrow function is executed by IIFE")
+})();
