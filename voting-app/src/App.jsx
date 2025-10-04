b8cf1ba998598da0b871a1caeab01ea97c2a7b16
shaikat020 <tahsin.azad.skt@gmail.com>
2025-10-04 17:13:41 +0600

    Feat: Put age condition for voting app

---
+++ voting-app/src/App.jsx
@@ -0,0 +1,40 @@
+// With useState:
+// It adds a state variable to a component.
+// State is data that can change over time (like input values, counters, toggles, fetched data).
+// When state changes, React re-renders the component with the new data
+
+import { useState } from "react";
+
+function App() {
+  // const hook = useState(0);
+  //using destructor
+  let [ age, setAge]= useState(15);
+  // console.log(a, b);
+  // let count= 0;
+  
+
+  return (
+    <>
+      <h1>Age is {age}</h1>
+      
+      <button onClick={()=> {
+        age++;
+        setAge(age);
+      
+    }
+      }>Inc</button>
+      <button onClick={()=> {
+        age--;
+        setAge(age);  
+      }
+      }>Dec</button>
+
+      <p>{age < 18 ? 'Sorry, you are not eligible to vote.': 'Welcome! You are eligible to vote.'}</p>
+      
+    </>
+  )
+}
+
+export default App
+
+// doesnt work in my ui view as react work as virtual dom 
