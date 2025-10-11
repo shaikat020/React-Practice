5b989656dd1eaf2986e97a57ee82a71d3171787b
shaikat020 <tahsin.azad.skt@gmail.com>
2025-10-11 16:36:45 +0600

    Feat: Create handlechange for managing onchange event

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -6,21 +6,16 @@ export const Frm = () => {
     const [age, setAge] = useState("22");
     const [gender, setgender] = useState("");  
     
+    const handleChange = (setState, value)=>{
+      console.log(value);
+      setState(value);
+    }
     
-  return <><input type="text" value={name}  onChange={(e) => {
-    console.log(e.target.value);
-    setName(e.target.value); 
-  }}/>
+  return <><input type="text" value={name}  onChange={(e) => handleChange(setName, e.target.value)}/>
   <br />
   <br />
-  <input type="number" value={age}  onChange={(e) => {
-    console.log(e.target.value);
-    setAge(e.target.value); 
-  }}/>
-  <select value={gender} onChange={(e)=>{
-    setgender(e.target.value);
-    console.log(e.target.value);
-  }}>
+  <input type="number" value={age}  onChange={(e) => handleChange(setAge, e.target.value)}/>
+  <select value={gender} onChange={(e)=> handleChange(setgender, e.target.value)}>
     <option value="" disabled>--Select--</option>
     <option value="Male">Male</option>
     <option value="Female">Female</option>
