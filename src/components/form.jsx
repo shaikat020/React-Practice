3ad591ea84f8a239b9aed84f26b941418f8cc8d0
shaikat020 <tahsin.azad.skt@gmail.com>
2025-10-11 15:40:09 +0600

    Feat: Using onchange event to remove readonly

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 
 export const Frm = () => {
     const [name, setName] = useState("");
-  return <input type="text" value="Shaikat" />;
+  return <input type="text" value="Shaikat"  onChange={() => {}}/>;
   
 }
+
+//This will result the input field will turn into read only
