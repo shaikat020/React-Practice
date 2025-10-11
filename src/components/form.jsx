bed2159a85454affbe6faeaec77f8b9c2f94117c
shaikat020 <tahsin.azad.skt@gmail.com>
2025-10-11 16:31:54 +0600

    Feat: Input field and option field

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,9 +3,33 @@ import { useState } from 'react';
 
 export const Frm = () => {
     const [name, setName] = useState("Shaikat");
-  return <input type="text" value={name}  onChange={(e) => {
+    const [age, setAge] = useState("22");
+    const [gender, setgender] = useState("");  
+    
+    
+  return <><input type="text" value={name}  onChange={(e) => {
     console.log(e.target.value);
-    setName(e.target.value);
-  }}/>;
+    setName(e.target.value); 
+  }}/>
+  <br />
+  <br />
+  <input type="number" value={age}  onChange={(e) => {
+    console.log(e.target.value);
+    setAge(e.target.value); 
+  }}/>
+  <select value={gender} onChange={(e)=>{
+    setgender(e.target.value);
+    console.log(e.target.value);
+  }}>
+    <option value="" disabled>--Select--</option>
+    <option value="Male">Male</option>
+    <option value="Female">Female</option>
+  </select>
+
+  <p>My name is {name}, I am {age} years old.</p>
+  </>
+  
+
+
   
 }
